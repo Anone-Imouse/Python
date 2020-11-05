3ecc7f5c194082f772417ce85d3a07f66465d358
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-11-05 16:09:26 -0500

    Update and rename what.py to zoom.js

---
+++ zoom.js
@@ -0,0 +1,2 @@
+/*Look, I need you to go in change view and click on the editor layout that's on the left. Or else, my creation looks like absolute trash.*/
+alert("Look, I need you to go in change view and click on the editor layout that's on the left. Or else, my creation looks like absolute trash.");
